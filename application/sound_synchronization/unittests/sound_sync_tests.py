5bf795f1efcc167853ae93fdf3341fe940010207
ulrich.aschl@gmail.com <ulrich.aschl@gmail.com>
2020-04-11 19:31:41 +0200

    sound sync algorithm

--- application/sound_synchronization/unittests/sound_sync_tests.py
+++ application/sound_synchronization/unittests/sound_sync_tests.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from application.sound_synchronization.sound_synchronization import get_error, synchronize, synchronize_multiple
+import soundfile as sf
 
 
 class MyTestCase(unittest.TestCase):
@@ -29,7 +30,32 @@ class MyTestCase(unittest.TestCase):
         for file in os.listdir("resources/out2/"):
             os.remove(os.path.join("resources/out2", file))
 
-        synchronize_multiple("resources/sync2.ogg", "resources/in2", "resources/out2", out_format="flac")
+        synchronize_multiple("resources/sync2.ogg", "resources/in2", "resources/out2", threads=1, out_format="flac")
+
+    def test_multisynchronize_real(self):
+        for file in os.listdir("resources/real_example/out/"):
+            os.remove(os.path.join("resources/real_example/out", file))
+
+        synchronize_multiple("resources/real_example/Master.ogg", "resources/real_example/in", "resources/real_example/out", threads=4, out_format="flac")
+
+    def test_multisynchronize_real01(self):
+        if os.path.exists("resources/real_example/33.flac"):
+            os.remove("resources/real_example/33.flac")
+
+
+        with sf.SoundFile("resources/real_example/Master.ogg", 'r') as sync_file:
+            sync_signal = sync_file.read(10 * sync_file.samplerate)
+            synchronize(sync_signal, sync_file.samplerate, "resources/real_example/in/33.ogg", "resources/real_example/33.flac", error_resample_div=10)
+        self.assertTrue(os.path.exists("resources/real_example/33.flac"))
+
+
+    def test_multisynchronize_real05(self):
+        if os.path.exists("resources/real_example/05.flac"):
+            os.remove("resources/real_example/05.flac")
+
+        with sf.SoundFile("resources/real_example/Master.ogg", 'r') as sync_file:
+            sync_signal = sync_file.read(10 * sync_file.samplerate)
+            synchronize(sync_signal, sync_file.samplerate, "resources/real_example/in/05.ogg", "resources/real_example/05.flac", error_resample_div=10)
 
 
 if __name__ == '__main__':
